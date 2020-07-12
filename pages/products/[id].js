a629e7a30b6839563576eb2bf3f41d011217150e
Emmanuel Sanchez <emmanuelsanchez@MacBook-Pro-de-erik-2.local>
2020-07-12 03:03:11 -0500

    left side of aside ready in Product id Ruoute component

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -3,6 +3,18 @@ import { useRouter } from "next/router";
 import { FirebaseContext } from "../../firebase";
 import Layout from "../../components/layouts/Layout";
 import Error404 from "../../components/layouts/404";
+import { css } from "@emotion/core";
+import styled from "@emotion/styled";
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
+import { Field, InputSubmit } from "../../components/ui/Form";
+
+const ProductContainer = styled.div`
+  @media (min-width: 768px) {
+    display: grid;
+    grid-template-columns: 2fr 1fr;
+    column-gap: 2rem;
+  }
+`;
 
 const Product = () => {
   //component state
@@ -34,9 +46,62 @@ const Product = () => {
     }
   }, [id]);
 
+  if (Object.keys(product).length === 0) return "Loading...";
+
+  const {
+    name,
+    enterprise,
+    urlImage,
+    url,
+    description,
+    votes,
+    createdAt,
+    comments,
+  } = product;
+
   return (
     <Layout>
       <Fragment>{error && <Error404 />}</Fragment>
+      <div className="contenedor">
+        <h1
+          css={css`
+            text-align: center;
+            margin-top: 5rem;
+          `}
+        >
+          {name}
+        </h1>
+        <ProductContainer>
+          <div>
+            <p> Published {formatDistanceToNow(new Date(createdAt))}</p>
+            <img src={urlImage} />
+            <p>{description}</p>
+
+            <h2>Add your comment</h2>
+            <form>
+              <Field>
+                <input type="text" name="message" />
+              </Field>
+              <InputSubmit type="submit" value="Add comment" />
+            </form>
+
+            <h2
+              css={css`
+                margin: 2rem 0;
+              `}
+            >
+              Comments
+            </h2>
+            {comments.map((comment) => (
+              <li>
+                <p>{comment.name}</p>
+                <p>Written by: {comment.username}</p>
+              </li>
+            ))}
+          </div>
+          <aside>2</aside>
+        </ProductContainer>
+      </div>
     </Layout>
   );
 };
