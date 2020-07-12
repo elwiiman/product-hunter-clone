5d14f319e5bacef3e07d811229655173fdf81b38
Emmanuel Sanchez <emmanuelsanchez@MacBook-Pro-de-erik-2.local>
2020-07-12 01:11:49 -0500

    listing products in home route / ready

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,47 @@
+import React, { useEffect, useState, useContext } from "react";
 import styled from "@emotion/styled";
 import Layout from "../components/layouts/Layout";
+import DetailProduct from "../components/layouts/DetailProduct";
+import { FirebaseContext } from "../firebase";
 
 const Home = () => {
+  const [products, setProducts] = useState([]);
+  const { firebase } = useContext(FirebaseContext);
+
+  useEffect(() => {
+    // const handleSnapshot = (snapshot) => {
+    //   const products = snapshot.docs.map((doc) => {
+    //     return { id: doc.id, ...doc.data() };
+    //   });
+    //   console.log(products);
+    // };
+    const obtainProducts = () => {
+      firebase.db
+        .collection("products")
+        .orderBy("createdAt", "desc")
+        .onSnapshot(handleSnapshot);
+    };
+    obtainProducts();
+  }, []);
+
+  function handleSnapshot(snapshot) {
+    const products = snapshot.docs.map((doc) => {
+      return { id: doc.id, ...doc.data() };
+    });
+    setProducts(products);
+  }
   return (
     <div>
       <Layout>
-        <h1>Home</h1>
+        <div className="listado-productos">
+          <div className="contenedor">
+            <ul className="bg-white">
+              {products.map((product) => (
+                <DetailProduct key={product.id} product={product} />
+              ))}
+            </ul>
+          </div>
+        </div>
       </Layout>
     </div>
   );
