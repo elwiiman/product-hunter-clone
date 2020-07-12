d38a460ba7630eb6ce220197233444ef996102b3
Emmanuel Sanchez <emmanuelsanchez@MacBook-Pro-de-erik-2.local>
2020-07-12 02:29:38 -0500

    dynamic routing for products id's working fine, showing 404 Error if a product id doesn't exist

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,6 +1,44 @@
-import React from "react";
+import React, { useEffect, useContext, useState, Fragment } from "react";
+import { useRouter } from "next/router";
+import { FirebaseContext } from "../../firebase";
+import Layout from "../../components/layouts/Layout";
+import Error404 from "../../components/layouts/404";
+
 const Product = () => {
-  return <h1>desde id js</h1>;
+  //component state
+  const [product, setProduct] = useState({});
+  const [error, setError] = useState(false);
+
+  //obtain the current product id
+  const router = useRouter();
+  const {
+    query: { id },
+  } = router;
+
+  //fire base context
+  const { firebase } = useContext(FirebaseContext);
+
+  useEffect(() => {
+    if (id) {
+      console.log("id available", id);
+      const obtainProduct = async () => {
+        const productQuery = await firebase.db.collection("products").doc(id);
+        const thisProduct = await productQuery.get();
+        if (thisProduct.exists) {
+          setProduct(thisProduct.data());
+        } else {
+          setError(true);
+        }
+      };
+      obtainProduct();
+    }
+  }, [id]);
+
+  return (
+    <Layout>
+      <Fragment>{error && <Error404 />}</Fragment>
+    </Layout>
+  );
 };
 
 export default Product;
